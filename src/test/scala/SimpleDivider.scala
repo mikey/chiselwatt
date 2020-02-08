9a8593f70fb5e86ee14c226f64b3c2fadc0511f4
Anton Blanchard <anton@linux.ibm.com>
2020-02-09 08:32:55 +1100

    Add divide extended tests
    Signed-off-by: Anton Blanchard <anton@linux.ibm.com>

--- src/test/scala/SimpleDivider.scala
+++ src/test/scala/SimpleDivider.scala
@@ -14,29 +14,40 @@ class SimpleDividerUnitTester extends FlatSpec with ChiselScalatestTester with M
     y <- testValuesRealShort
   } yield (x, y)
 
-  def divide(a: BigInt, b: BigInt): BigInt = (a / b) & BigInt("ffffffffffffffff", 16)
+  def divide(a: BigInt, b: BigInt): BigInt = (a / b)
 
-  def divideExtended(a: BigInt, b: BigInt): BigInt = (((a << 64) / b) >> 64) & BigInt("ffffffffffffffff", 16)
+  def divideExtended(a: BigInt, b: BigInt): BigInt = {
+    val d = ((a << 64) / b)
+    if ((d >> 64) == 0) d else 0
+  }
+
+  def runOneTest(m: SimpleDivider, divide: (BigInt, BigInt) => BigInt) = {
+    for ((x, y) <- tests) {
+
+      while (m.io.in.ready.peek().litToBoolean == false) {
+        m.clock.step(1)
+      }
+
+      m.io.in.bits.dividend.poke(x.U)
+      m.io.in.bits.divisor.poke(y.U)
+      m.io.in.valid.poke(true.B)
+      m.clock.step(1)
+      m.io.in.valid.poke(false.B)
+
+      while (m.io.out.valid.peek().litToBoolean == false) {
+        m.clock.step(1)
+      }
+
+      m.io.out.bits.expect(divide(x, y).U)
+    }
+  }
 
   it should "pass a unit test" in {
     test(new SimpleDivider(64)) { m =>
-      for ((x, y) <- tests) {
-        while (m.io.in.ready.peek().litToBoolean == false) {
-          m.clock.step(1)
-        }
+      runOneTest(m, divide)
 
-        m.io.in.bits.dividend.poke(x.U)
-        m.io.in.bits.divisor.poke(y.U)
-        m.io.in.valid.poke(true.B)
-        m.clock.step(1)
-        m.io.in.valid.poke(false.B)
-
-        while (m.io.out.valid.peek().litToBoolean == false) {
-          m.clock.step(1)
-        }
-
-        m.io.out.bits.expect(divide(x, y).U)
-      }
+      m.io.in.bits.extended.poke(true.B)
+      runOneTest(m, divideExtended)
     }
 
   }
